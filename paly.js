134786431279cec051bcce88ad08e9a6a272fd48
Yakir91 <lavikidd@gmail.com>
2022-11-03 22:45:28 +0200

    creating html and css page

---
+++ paly.js
@@ -0,0 +1,25 @@
+// Select the elements in the page
+// Set them into a variable.
+const testButton = document.querySelector('.test');
+const resetButton = document.querySelector('.reset');
+const inputName = document.querySelector('.field');
+
+testButton.addEventListener("click", function() {
+    palindrome();
+    }
+);
+
+function palindrome() {
+    
+}
+// test-flow
+    // User typing a word.
+    // User clicks 'Test' button
+//let function palindromeTest(
+    // Build palindrome function 
+
+
+        // if palindrome - button.background = green + success text output.
+        // else - failed text output + button.background = red.
+
+    // if user clicks reset - delete the value from the input field (whether there is or not)
